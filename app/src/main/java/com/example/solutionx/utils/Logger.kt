2ee8e29693dfcb38e007462b863eb9abc8af82fc
Sara Salah <93276124+SaraSallah@users.noreply.github.com>
2024-03-13 11:23:54 +0200

    add application

--- app/src/main/java/com/example/solutionx/utils/Logger.kt
+++ app/src/main/java/com/example/solutionx/utils/Logger.kt
@@ -6,38 +6,32 @@ import com.example.solutionx.BuildConfig
 import java.io.File
 import java.io.FileWriter
 import java.io.IOException
-import java.text.SimpleDateFormat
-import java.util.Date
-import java.util.Locale
 
 // Logger.kt
-
-class Logger {
-    companion object {
-        fun log(message: String) {
-            when (BuildConfig.FLAVOR) {
-                "logCat" -> logCat(message)
-                "logWriter" -> logWriter(message)
-                else -> {} // Do nothing for "production" flavor
-            }
+object Logger {
+    fun log(message: String) {
+        when (BuildConfig.FLAVOR) {
+            "logCat" -> logCat(message)
+            "logWriter" -> logWriter(message)
+            else -> {} // Do nothing for "production" flavor
         }
+    }
 
-        private fun logCat(message: String) {
-            Log.d("Sara", message)
-        }
+    private fun logCat(message: String) {
+        Log.d("Sara", message)
+    }
 
-        private fun logWriter(message: String) {
-            val directory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
-            val file = File(directory, "log.txt")
-            try {
-                val writer = FileWriter(file, true)
-                writer.append(message)
-                writer.append("\n")
-                writer.flush()
-                writer.close()
-            } catch (e: IOException) {
-                e.printStackTrace()
-            }
+    private fun logWriter(message: String) {
+        val directory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
+        val file = File(directory, "log.txt")
+        try {
+            val writer = FileWriter(file, true)
+            writer.append(message)
+            writer.append("\n")
+            writer.flush()
+            writer.close()
+        } catch (e: IOException) {
+            e.printStackTrace()
         }
     }
 }
